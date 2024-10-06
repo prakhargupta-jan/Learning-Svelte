2c4126d541d38c0e856d949e90892523a572b4cb
prakhargupta-jan <75901535+prakhargupta-jan@users.noreply.github.com>
2024-10-06 23:41:30 +0530

    Basic Svelte > Props > If Blocks

--- src/components/Nested.svelte
+++
@@ -1,8 +0,0 @@
-<script lang='ts'>
-    export let age: Number;
-    export let name: String
-    export let gender: "Male" | "Female"
-    export let property: "Hot" | "Ugly" | "Fat" | "Skinny" | "Black" | "White" | "Brown" | "Petite" | "Tall" | "Knockers";
-</script>
-
-<p>{name} is {age} years old, and {gender === "Male" ? "He" : "She"} is {property}</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,11 @@
 <script lang='ts'>
-    import Nested from "../components/Nested.svelte";
-    const person: {
-        name: String,
-        age: Number,
-        gender: "Male" | "Female",
-        property: "Hot" | "Ugly" | "Fat" | "Skinny" | "Black" | "White" | "Brown" | "Petite" | "Tall" | "Knockers";
-    } = {
-        name: "Ramesh",
-        age: 22,
-        gender: "Male",
-        property: "Black",
-    }
+    let count = 0;
+    const increment = () => count++;
 </script>
-<Nested {...person} />
+<button on:click={increment}>
+    Clicked {count}
+</button>
+
+{#if count > 10}
+    <p>{count} is greater than 10.</p>
+{/if}
