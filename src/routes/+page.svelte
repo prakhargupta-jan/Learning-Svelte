14f4c067ba264a01ce6860591daa0f7df8b134f3
prakhargupta-jan <75901535+prakhargupta-jan@users.noreply.github.com>
2024-10-06 22:44:41 +0530

    Basic Svelte > Reactivity > Updating arrays and objects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
-<script>
-    let count = 0;
-    const increment = () => count++;
-    $: if (count >= 10) {
-        alert('count is high');
-        count = 0;
+<script lang="ts">
+    let numbers: number[] = [];
+    let name = {
+        firstName: "Vijay",
+        lastName: "Sharma",
     }
-
+    const addNumber = () => numbers = [...numbers, numbers.length + 1];
+    const toggleName = () => name.lastName === "Sharma" ? name = {...name, lastName: "Gupta"} : name = {...name, lastName: "Sharma"};
+     
 </script>
-<button on:click={increment}>{count} {count > 1 ? 'time' : 'times'}</button>
+<button on:click={toggleName}>Toggle Name: {name.firstName} {name.lastName}</button>
+<button on:click={addNumber}>Add Number: {numbers.length}</button>
