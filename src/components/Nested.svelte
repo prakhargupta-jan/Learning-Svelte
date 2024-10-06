a1488936748438ff1f79f55c506910909cbc3c5e
prakhargupta-jan <75901535+prakhargupta-jan@users.noreply.github.com>
2024-10-06 23:02:11 +0530

    Basic Svelte > Props > Spread props

--- src/components/Nested.svelte
+++ src/components/Nested.svelte
@@ -1,5 +1,8 @@
-<script>
-    export let answer = "POOP";
+<script lang='ts'>
+    export let age: Number;
+    export let name: String
+    export let gender: "Male" | "Female"
+    export let property: "Hot" | "Ugly" | "Fat" | "Skinny" | "Black" | "White" | "Brown" | "Petite" | "Tall" | "Knockers";
 </script>
 
-<p>The answer is {answer}</p>
+<p>{name} is {age} years old, and {gender === "Male" ? "He" : "She"} is {property}</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
-<script>
+<script lang='ts'>
     import Nested from "../components/Nested.svelte";
+    const person: {
+        name: String,
+        age: Number,
+        gender: "Male" | "Female",
+        property: "Hot" | "Ugly" | "Fat" | "Skinny" | "Black" | "White" | "Brown" | "Petite" | "Tall" | "Knockers";
+    } = {
+        name: "Ramesh",
+        age: 22,
+        gender: "Male",
+        property: "Black",
+    }
 </script>
-<Nested answer="Ramesh" />
-<Nested />
+<Nested {...person} />
