71ae11b9e9a4f348f0a8f829606d80d4fbaaf0e6
prakhargupta-jan <75901535+prakhargupta-jan@users.noreply.github.com>
2024-10-06 16:33:07 +0530

    Basic Svelte > Reactivity > Statements

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    let count = 0;
+    const increment = () => count++;
+    $: if (count >= 10) {
+        alert('count is high');
+        count = 0;
+    }
+
+</script>
+<button on:click={increment}>{count} {count > 1 ? 'time' : 'times'}</button>
